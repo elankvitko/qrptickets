01b8fa862a90590e0c84601948c2a65ce2515125
Elan Kvitko <elan@idknyc.com>
2017-03-08 12:48:44 -0500

    done

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ApplicationMailer
-  def new_contact( ticket_number, name, email, body )
+  def new_contact( ticket_number, name, email, facility, body )
     @ticket_number = ticket_number
     @name = name
     @email = email
     @body = body
+    @facility = facility
 
     mail to: "ekvitko@qrpgroup.com",
          subject: 'New Ticket'
