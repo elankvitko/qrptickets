2d0d789dafa606d4e92e28f9a834a1457be951ff
Elan Kvitko <elan@idknyc.com>
2017-03-17 12:16:38 -0400

    added api ef fix

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,20 +5,21 @@ class ContactController < ApplicationController
 
       session = GoogleDrive::Session.from_config("config.json")
       ws = session.spreadsheet_by_key("1aYKq_4dC24KbZsM7oG76ziVE0hXnQLJUzf1PI5etks4").worksheets[0]
-      idx = ws.rows.count - 2
+      idx = ws.rows.count + 1
       val_idx = 1
 
       @ticket.attributes.each do | key, val |
         next if key == "id"
-        next if key == "created_at"
-        next if key == "updated_at"
-        
+        break if key == "created_at"
+
         if key == 'body'
           skip_idx = val_idx + 1
           ws[ idx, skip_idx ] = val
         elsif key == 'facility'
           prev_idx = val_idx - 1
           ws[ idx, prev_idx ] = val
+        elsif key == 'ticket_number'
+          ws[ idx, val_idx ] = idx + 4
         else
           ws[ idx, val_idx ] = val
         end
