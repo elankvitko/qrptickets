54d7a4f77ab901774a1db5d318f07fc8a169fea7
Elan Kvitko <elan@idknyc.com>
2017-03-17 12:05:18 -0400

    added

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -5,11 +5,24 @@ class ContactController < ApplicationController
 
       session = GoogleDrive::Session.from_config("config.json")
       ws = session.spreadsheet_by_key("1aYKq_4dC24KbZsM7oG76ziVE0hXnQLJUzf1PI5etks4").worksheets[0]
-      idx = ws.rows.count + 1
+      idx = ws.rows.count - 2
       val_idx = 1
-      
+
       @ticket.attributes.each do | key, val |
-        ws[ idx, val_idx ] = val
+        next if key == "id"
+        next if key == "created_at"
+        next if key == "updated_at"
+        binding.pry
+        if key == 'body'
+          skip_idx = val_idx + 1
+          ws[ idx, skip_idx ] = val
+        elsif key == 'facility'
+          prev_idx = val_idx - 1
+          ws[ idx, prev_idx ] = val
+        else
+          ws[ idx, val_idx ] = val
+        end
+
         val_idx += 1
       end
 
