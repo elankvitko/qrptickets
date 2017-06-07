08ff3c43984b879ffd01de9d973aad6c4d048d4a
Elan Kvitko <elan@idknyc.com>
2017-06-07 09:57:32 -0400

    fix excel bug

--- app/helpers/eworker_helper.rb
+++ app/helpers/eworker_helper.rb
@@ -4,7 +4,7 @@ module EworkerHelper
     purchasing_ndc_duplicates = {}
     found = {}
 
-    CSV.foreach( 'billing.csv', headers:true, :header_converters=> lambda {|f| f.strip}, :converters=> lambda {|f| f ? f.strip : nil} ) do | billing |
+    CSV.foreach( 'billing.csv', headers:true, :header_converters=> lambda { | f | f.strip }, :converters=> lambda { | f | f ? f.strip : nil } ) do | billing |
       if billing[ "CLASS" ].to_i > 0
         if billing_ndc_duplicates[ billing[ "NDC" ].gsub( '-', '' ) ]
           billing_ndc_duplicates[ billing[ "NDC" ].gsub( '-', '' ) ][ 5 ] += billing[ "QUANT" ].to_i
@@ -18,11 +18,11 @@ module EworkerHelper
     billing_ndc_duplicates.each { | k,v | v[ 4 ] = v[ 4 ].round( 2 ) }
 
 
-    CSV.foreach( 'purchasing.csv', headers:true, :header_converters=> lambda {|f| f.strip}, :converters=> lambda {|f| f ? f.strip : nil} ) do | purchasing |
-      if purchasing_ndc_duplicates[ purchasing[ "Universal Ndc #" ].gsub( '0', '' )  ]
-        purchasing_ndc_duplicates[ purchasing[ "Universal Ndc #" ].gsub( '0', '' )  ][ 3 ] += purchasing[ "Quantity Billed" ].to_i
+    CSV.foreach( 'purchasing.csv', headers:true, :header_converters=> lambda { | f | f.strip }, :converters=> lambda { | f | f ? f.strip : nil } ) do | purchasing |
+      if purchasing_ndc_duplicates[ purchasing[ "Universal Ndc #" ] ]
+        purchasing_ndc_duplicates[ purchasing[ "Universal Ndc #" ] ][ 3 ] += purchasing[ "Quantity Billed" ].to_i
       else
-        purchasing_ndc_duplicates[ purchasing[ "Universal Ndc #" ].gsub( '0', '' )  ] = [ purchasing[ "Item Description" ], purchasing[ "Item Type" ], purchasing[ "Size" ].to_i, purchasing[ "Quantity Billed" ].to_i ]
+        purchasing_ndc_duplicates[ purchasing[ "Universal Ndc #" ] ] = [ purchasing[ "Item Description" ], purchasing[ "Item Type" ], purchasing[ "Size" ].to_i, purchasing[ "Quantity Billed" ].to_i ]
       end
     end
 
@@ -34,7 +34,7 @@ module EworkerHelper
 
     billing_ndc_duplicates.each do | ndc, arr |
       purchasing_ndc_duplicates.each do | ndc2, arr2 |
-        if ndc.gsub( '0', '' ) == ndc2
+        if ndc.gsub( /(^|[^0-9])0+/, "" ) == ndc2
           complete = arr.insert( 5, arr2[ 2 ] )
           complete = complete.insert( 0, ndc )
           found[ ndc ] = complete
@@ -42,7 +42,7 @@ module EworkerHelper
       end
     end
 
-    CSV.open("file.csv", "wb", :write_headers=> true, :headers => [ "NDC", "Drug Name", "Drug Strength", "Package Size", "Control", "Total Ins. Paid", "Total Units Purchased", "Total Units Billed" ]) do |csv|
+    CSV.open( "file.csv", "wb", :write_headers=> true, :headers => [ "NDC", "Drug Name", "Drug Strength", "Package Size", "Control", "Total Ins. Paid", "Total Units Purchased", "Total Units Billed" ] ) do | csv |
       found.each do | ndc, arr |
         csv << arr
       end
